efcb7724841d089059c85cd36d96e5def0184801
Olle Wikström <olle.wikstrom@sogeti.se>
2025-01-29 09:43:42 +0100

    new build steps with prerender false

--- src/routes/match/[match_group_id]/+page.server.ts
+++ src/routes/match/[match_group_id]/+page.server.ts
@@ -1,10 +1,14 @@
+export const prerender = false;
+
 import { API_URL } from '$env/static/private';
 
 export async function load({ params }: { params: { match_group_id: string } }) {
 	const match_group_id = params.match_group_id;
 	console.log('match_group_id', match_group_id);
 
-	const matchData = await fetch(API_URL + '/match_group/' + match_group_id).then((res) => res.json());
+	const matchData = await fetch(API_URL + '/match_group/' + match_group_id).then((res) =>
+		res.json()
+	);
 	console.log('matchData', matchData);
 
 	return {
