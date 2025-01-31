670fbd8ebf25cd9ab464e8cb1e2082257b2d08ef
Israa Aly Abdou <israa.aly-abdou@sogeti.com>
2025-01-31 16:17:54 +0100

    added buttons

--- src/routes/match/[match_group_id]/+page.svelte
+++ src/routes/match/[match_group_id]/+page.svelte
@@ -1,14 +1,23 @@
 <script lang="ts">
 	import * as Tabs from '$lib/components/ui/tabs';
-	import { Tooltip } from 'flowbite-svelte';
+	import { Button, ButtonGroup, Tooltip } from 'flowbite-svelte';
+	//import SvelteCopyUrlButton from 'svelte-copy-url-button';
 	import { match } from 'effect/ChildExecutorDecision';
+	import { url } from 'effect/Config';
 	let { data } = $props();
+	//let {jobData} = $props();
 	let { matchData } = data;
 	let { match_group_id, job_listing_url, matches, job_listing_name } = matchData;
 	console.log(matchData);
 
-	let placement = 'top';
+	let placeholderText = '';
 
+	function handleClick() {
+		placeholderText = 'Processing...';
+		setTimeout(() => {
+			placeholderText = '';
+		}, 3000); // Reset after 3 seconds
+	}
 	const matchType = new Map([
 		['MATCH', `list-['✅']`],
 		['PARTIAL MATCH', `list-['☑️']`],
@@ -27,14 +36,33 @@
 {#await data}
 	<p>Loading...</p>
 {:then data}
-	<div class="flex justify-between p-8">
-		<a href={job_listing_url} target="_blank" class="text-xl text-blue-500 underline"
-			>View Job Listing</a
-		>
+	<div class="flex w-full flex-col pl-8 pr-8 pt-8">
+		<ButtonGroup class="*:!ring-primary-700  ml-auto">
+			<Button
+				on:click
+				class="rounded-bl-md rounded-tl-md bg-blue-100   hover:bg-blue-300 hover:text-white hover:shadow-2xl"
+				><a href={job_listing_url} target="_blank">Prospect</a></Button
+			>
+			<Tooltip class="opacity-2 gap-y-10 bg-slate-600 text-white">Go to the job listing</Tooltip>
+			<Button
+				class="rounded-br-md rounded-tr-md bg-blue-100 hover:bg-blue-300 hover:text-white"
+				on:click={() => {
+					navigator.clipboard
+						.writeText(window.location.href)
+						.then(() => {
+							alert('URL copied to clipboard!');
+						})
+						.catch((err) => {
+							console.error('Failed to copy: ', err);
+						});
+				}}>Copy link</Button
+			>
+			<Tooltip class="opacity-2 gap-y-10 bg-slate-600 text-white">Copy URL to clipboard</Tooltip>
+		</ButtonGroup>
 	</div>
 
-	<Tabs.Root value={matches[0].match_id} class=" p-8 ">
-		<Tabs.List class="flex h-fit flex-wrap gap-2 bg-blue-100 p-4 text-slate-600">
+	<Tabs.Root value={matches[0].match_id} class=" p-8 pt-4">
+		<Tabs.List class="flex h-fit flex-wrap gap-2 bg-blue-100 p-2 text-slate-600">
 			{#each matches as match}
 				<Tabs.Trigger
 					id="tabs-button"
@@ -62,13 +90,13 @@
 						<h1 class="text-2xl">Required</h1>
 						{#each match.skills as skill}
 							{#if skill.level_of_importance == 'MUST HAVE'}
-								<ul class={'list-inside p-1 ' + matchType.get(skill.match_label)}>
-									<li class="text-xl capitalize">{skill.skill_name}</li>
-									<Tooltip class="opacity-2 delay justify-start bg-slate-600 text-white">
+								<ul class={'flex list-inside  p-1 ' + matchType.get(skill.match_label)}>
+									<li class=" text-xl capitalize">{skill.skill_name}</li>
+									<Tooltip class=" bg-slate-600  text-white">
 										<span> {matchTypeDesc.get(skill.match_label)}</span>
 									</Tooltip>
-									<p class="text-md">{skill.reason}</p>
 								</ul>
+								<p class="text-md pb-2">{skill.reason}</p>
 							{/if}
 						{/each}
 					</div>
@@ -79,7 +107,7 @@
 							{#if skill.level_of_importance != 'MUST HAVE'}
 								<ul class={'list-inside p-1 ' + matchType.get(skill.match_label)}>
 									<li class="text-xl capitalize">{skill.skill_name}</li>
-									<Tooltip class="opacity-2 justify-start bg-slate-600 text-white">
+									<Tooltip placement="left" class="opacity-2  bg-slate-600 text-white">
 										{matchTypeDesc.get(skill.match_label)}
 									</Tooltip>
 									<p class="text-md">{skill.reason}</p>
